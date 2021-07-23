74c71c9aed65efb3de4f080ef8422ced77c94d69
it-reader <hnature@126.com>
2021-07-24 00:34:33 +0800

    dir: pages config ...

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,10 @@ import router from './router/index';
 import store from './store/index';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import V from '@/mixins/v';
 
 Vue.config.productionTip = false;
 
 Vue.use(ElementUI);
-
-Vue.component('V', V);
 
 new Vue({
   router,
--- src/mixins/v/index.ts
+++
@@ -1,3 +0,0 @@
-import V from './V.vue'
-
-export default V;
