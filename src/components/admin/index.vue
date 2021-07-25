1806597f4bda2d9fee3204de0106eebe437a342c
it-reader <hnature@126.com>
2021-07-25 21:09:37 +0800

    admin-component

---
+++ src/components/admin/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <component :is="c">
+</template>
+
+<script lang="ts">
+import { Component, Mixins } from 'vue-property-decorator'
+import AdminState from './state'
+import V from '@/mixins/v'
+
+@Component({})
+export default class Admin extends Mixins(V) {
+  get state(): AdminState {
+    return this.$state as AdminState
+  }
+
+  get c() {
+    return {
+      render: (h: Function) => {
+        return h(this.state.type, {
+          props: {
+            mapping: this.getChildrenMapping('state')
+          }
+        })
+      }
+    }
+  }
+}
+</script>
